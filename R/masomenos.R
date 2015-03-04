69dc9132ec1f0f8c5948c666bb75f7e1880dac86
ttriche <tim.triche@gmail.com>
2015-03-03 17:13:15 -0800

    typo fix

--- R/masomenos.R
+++ R/masomenos.R
@@ -2,5 +2,5 @@ masomenos <- function(X, y, normalize=FALSE, digits=NULL, ...) {
   X <- data.frame(X, row.names=NULL)
   if (!is(y, 'Surv')) y <- Surv(y[, 1], y[, 2])
   alpha <- rowCoxTests(t(X), y, ...) ## design?
-  getCoefBetas(alpha, normalize=normalized, digits=digits)
+  getCoefBetas(alpha, normalize=normalize, digits=digits)
 }
